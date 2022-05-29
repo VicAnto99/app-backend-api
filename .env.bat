e52aadafc7f3768b177edc4a19342343ce96d3a5
VicAnto99 <A01339529@itesm.mx>
2022-05-29 17:03:28 -0500

    Fix

--- .env.bat
+++ .env.bat
@@ -2,5 +2,5 @@
  set CLOUD_API_KEY=Test
  set CLOUD_API_SECRET=Test
  set SECRET_KEY=SECRET_KEY
- #set DATABASE_URL=postgres://zjbssrjyjrhhrj:334695a17a476df55c7629b1c1957dbd518f55143e1a78d74fd44ef58494770f@ec2-44-196-174-238.compute-1.amazonaws.com:5432/d9rar31rvu0565
+ set DATABASE_URL=postgres://tqrdsfamyfcwpg:b5290434c4f3b53ca4d30690f78644401849177bcef398b7fca510ea0493e9da@ec2-44-196-174-238.compute-1.amazonaws.com:5432/dd4u5h9h3fvnt6
  

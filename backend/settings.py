82c2d1e52fef7f9cf3ba6622201776ba2874ce4f
VicAnto99 <A01339529@itesm.mx>
2022-05-29 18:55:32 -0500

    Fix

--- backend/settings.py
+++ backend/settings.py
@@ -91,7 +91,7 @@ ROOT_URLCONF = 'backend.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS':  [os.path.join(BASE_DIR, 'templates')],
+        'DIRS':  [os.path.join(BASE_DIR, 'Templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

0f73e5b138f69e0d5154c02ecee58bffd03e8312
VicAnto99 <A01339529@itesm.mx>
2022-05-29 17:53:25 -0500

    Fix

--- backend/settings.py
+++ backend/settings.py
@@ -124,7 +124,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default':{
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)

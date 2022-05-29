ebfef41e13e616509ddca8fada9ae2e673772b7c
VicAnto99 <A01339529@itesm.mx>
2022-05-29 16:32:10 -0500

    Fix

---
+++ .env.bat
@@ -0,0 +1,6 @@
+ set CLOUD_NAME=Test
+ set CLOUD_API_KEY=Test
+ set CLOUD_API_SECRET=Test
+ set SECRET_KEY=SECRET_KEY
+ set DATABASE_URL=postgres://zjbssrjyjrhhrj:334695a17a476df55c7629b1c1957dbd518f55143e1a78d74fd44ef58494770f@ec2-44-196-174-238.compute-1.amazonaws.com:5432/d9rar31rvu0565
+ 
--- backend/settings.py
+++ backend/settings.py
@@ -118,6 +118,8 @@ DATABASES = {
     },
 
 }
+DATABASES['default'] = dj_database_url.config(conn_max_age=600, ssl_require=True)
+
 
 
 

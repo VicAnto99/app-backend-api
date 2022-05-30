8b0abceb80b3993d5e35859a975f25171ed40e89
VicAnto99 <A01339529@itesm.mx>
2022-05-29 22:28:57 -0500

    Fix

--- backend/settings.py
+++ backend/settings.py
@@ -189,9 +189,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'build/static')
-]
+#STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, 'build/static')
+#]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
